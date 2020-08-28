06c572848e81e6ab7721128d3668abb723724abf
jorduque16 <jorge.duque@vendamasfacil.com>
2020-08-27 19:30:17 -0500

    Sqs integrated without lambda permissions.

--- handler.js
+++ handler.js
@@ -1,16 +1,36 @@
 'use strict';
 
-const { uuid } = require('uuidv4');
+const { v4: uuidv4 } = require('uuid');
+var AWS = require('aws-sdk');
 
-module.exports.makeAnOrder = async event => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: `Your order has been created with id: ${uuid()}`
-      },
-      null,
-      2
-    ),
+var sqs = new AWS.SQS({ region: process.env.REGION });
+var QUEUE_URL = process.env.PENDING_ORDER_QUEUE;
+
+module.exports.makeAnOrder = (event, context, callback) => {
+  const orderId = uuidv4();
+
+  const params = {
+    MessageBody: JSON.stringify({orderId: orderId}),
+    QueueUrl: QUEUE_URL
   };
+
+  sqs.sendMessage(params, function(err, data) {
+    if (err) {
+      sendResponse(500, err, callback);
+    } else {
+      const message = {
+          orderId: orderId,
+          messageId: data.MessageId
+      };
+      sendResponse(200, message, callback);
+    }
+  });
 };
+
+function sendResponse(statusCode, message, callback) {
+  const response = {
+      statusCode: statusCode,
+      body: JSON.stringify(message)
+  }
+  callback(null, response)
+}
